f129be91d5589b0e1a47cd24a7994f398326214a
Philip Williamson <williamson.philipandrew@gmail.com>
2021-07-18 17:50:19 -0400

    finish elaborate scene

--- src/util/hooks.ts/use-canvas-ref2.ts
+++ src/util/hooks.ts/use-canvas-ref2.ts
@@ -8,6 +8,19 @@ interface SceneParameters {
 	object?: string;
 }
 
+function addStars(scene) {
+	const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+	const material = new THREE.MeshBasicMaterial({ color: `#ffffff` });
+	const star = new THREE.Mesh(geometry, material);
+
+	const [x, y, z] = Array.from({ length: 3 }).map(() =>
+		THREE.MathUtils.randFloatSpread(100)
+	);
+
+	star.position.set(x, y, z);
+	scene.add(star);
+}
+
 const useCanvasRef2 = ({ object }: SceneParameters): ThreeJsCanvasRef => {
 	const canvasRef = useRef(null);
 
@@ -20,7 +33,7 @@ const useCanvasRef2 = ({ object }: SceneParameters): ThreeJsCanvasRef => {
 			0.1,
 			1000
 		);
-		const geometry = new THREE.TorusGeometry(5, 1, 15, 15);
+		const geometry = new THREE.TorusGeometry(5, 1, 16, 100);
 		const material = new THREE.MeshStandardMaterial({ color: `#ff1100` });
 		const mesh = new THREE.Mesh(geometry, material);
 
@@ -35,8 +48,41 @@ const useCanvasRef2 = ({ object }: SceneParameters): ThreeJsCanvasRef => {
 		scene.add(ambientlight);
 
 		scene.add(mesh);
+
+		// eslint-disable-next-line unicorn/no-array-for-each
+		Array.from({ length: 200 }).forEach(() => addStars(scene));
+
+		const spaceTexture = new THREE.TextureLoader().load(`/space.jpg`);
+		scene.background = spaceTexture;
+
+		const philTexture = new THREE.TextureLoader().load(`/headshot.jpg`);
+
+		const phil = new THREE.Mesh(
+			new THREE.BoxGeometry(3, 3, 3),
+			new THREE.MeshBasicMaterial({ map: philTexture })
+		);
+
+		scene.add(phil);
+
+		const moonTexture = new THREE.TextureLoader().load(`/moon.jpg`);
+		const normalTexture = new THREE.TextureLoader().load(`/normal.jpg`);
+
+		const moon = new THREE.Mesh(
+			new THREE.SphereGeometry(3, 32, 32),
+			new THREE.MeshStandardMaterial({
+				map: moonTexture,
+				normalMap: normalTexture,
+			})
+		);
+
+		scene.add(moon);
+
+		moon.position.set(10, 10, -10);
+
+		pointlight.position.set(10, 10, 10);
+
 		// DOLLY
-		camera.position.z = 15;
+		camera.position.x = -15;
 
 		const renderer = new THREE.WebGLRenderer();
 
